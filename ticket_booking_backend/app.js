67a7b47caedd9982412c8c89270eedc2c2a1a314
vrund-cpp <vrundleuva3@gmail.com>
2025-06-02 19:04:18 +0530

    comment temporary code used in backend deployment

--- ticket_booking_backend/app.js
+++ ticket_booking_backend/app.js
@@ -7,26 +7,26 @@ const app = express();
 
 const { exec } = require('child_process');
 
-app.get('/push-schema', async (req, res) => {
-  exec('npx prisma db push', (err, stdout, stderr) => {
-    if (err) {
-      console.error(stderr);
-      return res.status(500).send('Error pushing schema:\n' + stderr);
-    }
-    res.send('Schema pushed successfully:\n' + stdout);
-  });
-});
+// app.get('/push-schema', async (req, res) => {
+//   exec('npx prisma db push', (err, stdout, stderr) => {
+//     if (err) {
+//       console.error(stderr);
+//       return res.status(500).send('Error pushing schema:\n' + stderr);
+//     }
+//     res.send('Schema pushed successfully:\n' + stdout);
+//   });
+// });
 
-app.get('/push-schema', async (req, res) => {
-  const { exec } = require('child_process');
-  exec('npx prisma db push', (err, stdout, stderr) => {
-    if (err) {
-      console.error(stderr);
-      return res.status(500).send('Error pushing schema');
-    }
-    res.send('Schema pushed successfully:\n' + stdout);
-  });
-});
+// app.get('/push-schema', async (req, res) => {
+//   const { exec } = require('child_process');
+//   exec('npx prisma db push', (err, stdout, stderr) => {
+//     if (err) {
+//       console.error(stderr);
+//       return res.status(500).send('Error pushing schema');
+//     }
+//     res.send('Schema pushed successfully:\n' + stdout);
+//   });
+// });
 
 app.use(cors());
 app.use(express.json());
