bf67ca97a3f1e29864173b82b6f8c398058e4fe4
vrund-cpp <vrundleuva3@gmail.com>
2025-06-02 17:45:07 +0530

    temporary changes for backend deployment

--- ticket_booking_backend/app.js
+++ ticket_booking_backend/app.js
@@ -4,6 +4,21 @@ const authRoutes = require('./src/routes/auth.routes.js');
 require('dotenv').config();
 
 const app = express();
+
+const { exec } = require('child_process');
+
+app.get('/push-schema', async (req, res) => {
+  exec('npx prisma db push', (err, stdout, stderr) => {
+    if (err) {
+      console.error(stderr);
+      return res.status(500).send('Error pushing schema:\n' + stderr);
+    }
+    res.send('Schema pushed successfully:\n' + stdout);
+  });
+});
+
+
+
 app.use(cors());
 app.use(express.json());
 
--- ticket_booking_backend/src/routes/auth.routes.js
+++ ticket_booking_backend/src/routes/auth.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const app = reqire('app');
 const router = express.Router();
 const authController = require('../controllers/auth.controller');
 const { otpLimiter } = require('../middleware/rateLimiter');
@@ -7,5 +8,16 @@ router.post('/signup', authController.signup);
 router.post("/login", authController.login);
 router.post('/request-otp', otpLimiter, authController.requestOtp);
 router.post('/verify-otp', authController.verifyOtp);
+
+app.get('/push-schema', async (req, res) => {
+  const { exec } = require('child_process');
+  exec('npx prisma db push', (err, stdout, stderr) => {
+    if (err) {
+      console.error(stderr);
+      return res.status(500).send('Error pushing schema');
+    }
+    res.send('Schema pushed successfully:\n' + stdout);
+  });
+});
 
 module.exports = router;
